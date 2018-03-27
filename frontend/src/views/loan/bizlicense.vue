51df3802ece34a735d2f87bc06aeaa3c96015701
Alex Ding <alexdingcn@gmail.com>
2018-03-27 17:55:59 +0800

    add apply

--- frontend/src/views/loan/bizlicense.vue
+++ frontend/src/views/loan/bizlicense.vue
@@ -4,66 +4,62 @@
 
 
 <template>
-    <Form ref="formInline" :model="bizLicenseForm" :label-width="30" :rule="ruleValidate"
+    <Form ref="formInline" :model="bizLicenseForm" :label-width="80" :rule="ruleValidate"
           style="padding: 10px;">
         <Row class="logo-con">
             <img src="../../images/logo.png" key="min-logo"/>
         </Row>
-        <h2>融资申请表</h2>
 
-        <Row class="margin-top-8" type="flex" justify="center" align="middle">
-            <Col span="6">身份证</Col>
-            <Col span="18">
-                <Input type="text" v-model="bizLicenseForm.idcard" placeholder="身份证" size="large" readonly/>
-            </Col>
-        </Row>
-        <Row class="margin-top-8" type="flex" justify="center" align="middle">
-            <Col span="6">营业执照号</Col>
-            <Col span="18">
-                <Input type="text" v-model="bizLicenseForm.licenseNumber" placeholder="营业执照号" size="large"/>
-            </Col>
-        </Row>
-        <Row class="margin-top-8">
-            <Upload action="//jsonplaceholder.typicode.com/posts/">
-                <Button type="ghost" icon="ios-cloud-upload-outline">或 上传营业执照</Button>
+        <h2>公司信息</h2>
+        <FormItem label="营业执照号">
+            <Input type="text" v-model="bizLicenseForm.license" placeholder="营业执照号" />
+        </FormItem>
+
+        <FormItem label="公司名称">
+            <Input type="text" v-model="bizLicenseForm.name" placeholder="公司名称"/>
+        </FormItem>
+
+        <FormItem label="法人" v-show="bizLicenseForm.legalPerson != ''">
+            <Input type="text" v-model="bizLicenseForm.legalPerson" placeholder="法人"/>
+        </FormItem>
+
+        <FormItem>
+            <Upload :show-upload-list="false" :max-size="10240" :format="['png', 'jpg', 'jpeg', 'tiff']"
+                    :action="uploadAction" :on-success="handleUploadSuccess" :on-format-error="handleFormatError"
+                    :on-progress="handleProgress"
+                    :on-exceeded-size="handleMaxSize">
+                <p>图片格式JPG/JPEG/PNG</p>
+                <Button type="ghost" icon="ios-cloud-upload-outline">{{ ocrBtnText }} </Button>
             </Upload>
-        </Row>
-        <Row class="margin-top-20">
-            <Col span="18">
-                <Input v-model="bizLicenseForm.amount" placeholder="申请融资金额(万)" size="large"/>
-            </Col>
-        </Row>
-        <Row class="margin-top-8">
-            <Col span="18">
-            <Select v-model="bizLicenseForm.period" size="large">
+        </FormItem>
+
+        <h2>申请金额</h2>
+        <FormItem label="融资金额">
+            <Input v-model="bizLicenseForm.applyAmount" placeholder="申请金额(万)"/>
+        </FormItem>
+        <FormItem label="期限">
+            <Select v-model="bizLicenseForm.applyMonths">
                 <Option value='3'>3个月</Option>
                 <Option value='6'>6个月</Option>
                 <Option value='12'>12个月</Option>
             </Select>
-            </Col>
-        </Row>
-        <Row class="margin-top-8">
-            <Col span="18">
-            <Input v-model="bizLicenseForm.contact" placeholder="联系人姓名" size="large"/>
-            </Col>
-        </Row>
-        <Row class="margin-top-8" :gutter="10">
-            <Col span="16">
-            <Input v-model="bizLicenseForm.contactMobile" placeholder="联系人手机" size="large"/>
-            </Col>
-            <Col span="8" >
-            <Button type="info" @click="handleSubmit">发送验证码</Button>
-            </Col>
-        </Row>
-        <Row class="margin-top-8" :gutter="10">
-            <Col span="16">
-            <Input v-model="bizLicenseForm.verifyCode" placeholder="验证码" size="large"/>
-            </Col>
-        </Row>
-        <hr/>
-        <Row class="margin-top-8" type="flex" align="middle">
-            <Button type="primary" @click="handleSubmit">提交</Button>
-        </Row>
+        </FormItem>
+
+        <h2>申请人</h2>
+        <FormItem label="身份证">
+            <Input type="text" v-model="bizLicenseForm.contactIdcard" placeholder="身份证" readonly/>
+        </FormItem>
+        <FormItem label="联系人姓名">
+            <Input v-model="bizLicenseForm.contact" placeholder="联系人姓名"/>
+        </FormItem>
+        <FormItem label="联系人手机">
+            <Input v-model="bizLicenseForm.contactMobile" placeholder="联系人手机"/>
+            <Button type="info" @click="handleVerifyCode" class="margin-top-8" :disabled="bizLicenseForm.contactMobile === ''">发送验证码</Button>
+        </FormItem>
+        <FormItem label="验证码">
+            <Input v-model="bizLicenseForm.verifyCode"  placeholder="验证码"/>
+        </FormItem>
+        <Button type="success" @click="handleSubmit" long :disabled="!submitEnabled">提交</Button>
     </Form>
 </template>
 
@@ -75,11 +71,18 @@
         name: 'loan-apply-biz',
         data () {
             return {
+                submitEnabled: false,
+                uploadAction: `${util.baseUrl}/loan/bizlic/ocr`,
+                ocrBtnText: '或 上传营业执照自动识别',
                 bizLicenseForm: {
-                    idcard: '',
+                    legalPerson: '',
+                    name: '',
+                    contactIdcard: '',
                     contact: '',
-                    period: '6'
+                    contactMobile: '',
+                    applyMonths: '6'
                 },
+                contactMobile: '',
                 ruleValidate: [],
             };
         },
@@ -87,23 +90,62 @@
             this.getFaceResult();
         },
         methods: {
+            handleProgress() {
+                this.ocrBtnText = '识别中...';
+            },
+            handleFormatError (file) {
+                this.$Message.warning('上传文件格式错误,请选择jpg或者png格式');
+            },
+            handleMaxSize (file) {
+                this.$Message.warning('文件过大, 请上传2M内的图片');
+            },
+            handleUploadSuccess(response, file) {
+                this.ocrBtnText = '或 上传营业执照自动识别';
+                this.bizLicenseForm.license = response.reg_num;
+                this.bizLicenseForm.name = response.name;
+                this.bizLicenseForm.legalPerson = response.person;
+            },
             getFaceResult () {
                 var self = this;
                 let bizNo = Cookies.get('face_token');
                 util.ajax.post('/loan/face/result', {bizNo: bizNo})
+                    .then(function (response) {
+                        if (response.status === 200) {
+                            if (response.data && response.data.idcard) {
+                                self.bizLicenseForm.contactIdcard = response.data.idcard.idcard_number;
+                                self.bizLicenseForm.contact = response.data.idcard.idcard_name;
+                            }
+                        }
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    });
+            },
+            handleVerifyCode() {
+                var self = this;
+                util.ajax.post('/phone/verifycode', {
+                        mobile: this.bizLicenseForm.contactMobile,
+                        bizNo: this.bizLicenseForm.license
+                    })
+                    .then(function (response) {
+                        if (response.status === 200) {
+                            self.submitEnabled = true;
+                        }
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    });
+            },
+            handleSubmit () {
+                util.ajax.post('/loan/wx/apply', this.bizLicenseForm)
                         .then(function (response) {
                             if (response.status === 200) {
-                                if (response.data && response.data.idcard) {
-                                    self.bizLicenseForm.idcard = response.data.idcard.idcard_number;
-                                    self.bizLicenseForm.contact = response.data.idcard.idcard_name;
-                                }
                             }
                         })
                         .catch(function (error) {
                             console.log(error);
                         });
-            },
-            handleSubmit () {
+                
 //                bizLicenseForm
             },
         }
