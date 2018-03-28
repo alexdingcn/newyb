29d115a1f4f9bddcdda07c6a68c908e4d7ada17d
Alex Ding <alexdingcn@gmail.com>
2018-03-28 15:35:39 +0800

    update package.json

--- frontend/src/views/loan/bizlicense.vue
+++ frontend/src/views/loan/bizlicense.vue
@@ -7,63 +7,70 @@
 
 
 <template>
-    <Form ref="formInline" :model="bizLicenseForm" :label-width="80" :rule="ruleValidate"
-          style="padding: 10px;">
-        <Row class="logo-con">
-            <img src="../../images/logo.png" key="min-logo"/>
-        </Row>
+    <div>
+        <Form ref="formInline" :model="bizLicenseForm" :label-width="80" :rule="ruleValidate"
+              style="padding: 10px;" v-show="formShow">
+            <Row class="logo-con">
+                <img src="../../images/logo.png" key="min-logo"/>
+            </Row>
 
-        <h2>公司信息</h2>
-        <FormItem label="营业执照号">
-            <Input type="text" v-model="bizLicenseForm.license" placeholder="营业执照号" />
-        </FormItem>
+            <h2>公司信息</h2>
+            <FormItem label="营业执照号">
+                <Input type="text" v-model="bizLicenseForm.license" placeholder="营业执照号" />
+            </FormItem>
 
-        <FormItem label="公司名称">
-            <Input type="text" v-model="bizLicenseForm.name" placeholder="公司名称"/>
-        </FormItem>
+            <FormItem label="公司名称">
+                <Input type="text" v-model="bizLicenseForm.name" placeholder="公司名称"/>
+            </FormItem>
 
-        <FormItem label="法人" v-show="bizLicenseForm.legalPerson != ''">
-            <Input type="text" v-model="bizLicenseForm.legalPerson" placeholder="法人"/>
-        </FormItem>
+            <FormItem label="法人" v-show="bizLicenseForm.legalPerson != ''">
+                <Input type="text" v-model="bizLicenseForm.legalPerson" placeholder="法人"/>
+            </FormItem>
 
-        <FormItem>
-            <Upload :show-upload-list="false" :max-size="10240" :format="['png', 'jpg', 'jpeg', 'tiff']"
-                    :action="uploadAction" :on-success="handleUploadSuccess" :on-format-error="handleFormatError"
-                    :on-progress="handleProgress"
-                    :on-exceeded-size="handleMaxSize">
-                <p>图片格式JPG/JPEG/PNG</p>
-                <Button type="ghost" icon="ios-cloud-upload-outline">{{ ocrBtnText }} </Button>
-            </Upload>
-        </FormItem>
+            <FormItem>
+                <Upload :show-upload-list="false" :max-size="10240" :format="['png', 'jpg', 'jpeg', 'tiff']"
+                        :action="uploadAction" :on-success="handleUploadSuccess" :on-format-error="handleFormatError"
+                        :on-progress="handleProgress"
+                        :on-exceeded-size="handleMaxSize">
+                    <p>图片格式JPG/JPEG/PNG</p>
+                    <Button type="ghost" icon="ios-cloud-upload-outline">{{ ocrBtnText }} </Button>
+                </Upload>
+            </FormItem>
 
-        <h2>申请金额</h2>
-        <FormItem label="融资金额">
-            <Input v-model="bizLicenseForm.applyAmount" placeholder="申请金额(万)"/>
-        </FormItem>
-        <FormItem label="期限">
-            <Select v-model="bizLicenseForm.applyMonths">
-                <Option value='3'>3个月</Option>
-                <Option value='6'>6个月</Option>
-                <Option value='12'>12个月</Option>
-            </Select>
-        </FormItem>
+            <h2>申请金额</h2>
+            <FormItem label="融资金额">
+                <Input v-model="bizLicenseForm.applyAmount" placeholder="申请金额(万)"/>
+            </FormItem>
+            <FormItem label="期限">
+                <Select v-model="bizLicenseForm.applyMonths">
+                    <Option value='3'>3个月</Option>
+                    <Option value='6'>6个月</Option>
+                    <Option value='12'>12个月</Option>
+                </Select>
+            </FormItem>
 
-        <h2>申请人</h2>
-        <FormItem label="身份证">
-            <Input type="text" v-model="bizLicenseForm.contactIdcard" placeholder="身份证" readonly/>
-        </FormItem>
-        <FormItem label="联系人姓名">
-            <Input v-model="bizLicenseForm.contact" placeholder="联系人姓名"/>
-        </FormItem>
-        <FormItem label="联系人手机">
-            <Input v-model="bizLicenseForm.contactMobile" placeholder="联系人手机"/>
-            <Button type="info" @click="handleVerifyCode" class="margin-top-8" :disabled="bizLicenseForm.contactMobile === ''">发送验证码</Button>
-        </FormItem>
-        <FormItem label="验证码">
-            <Input v-model="bizLicenseForm.verifyCode"  placeholder="验证码"/>
-        </FormItem>
-        <Button type="success" @click="handleSubmit" long :disabled="!submitEnabled">提交</Button>
-    </Form>
+            <h2>申请人</h2>
+            <FormItem label="身份证">
+                <Input type="text" v-model="bizLicenseForm.contactIdcard" placeholder="身份证" readonly/>
+            </FormItem>
+            <FormItem label="联系人姓名">
+                <Input v-model="bizLicenseForm.contact" placeholder="联系人姓名"/>
+            </FormItem>
+            <FormItem label="联系人手机">
+                <Input v-model="bizLicenseForm.contactMobile" placeholder="联系人手机"/>
+                <Button type="info" @click="handleVerifyCode" class="margin-top-8" :disabled="bizLicenseForm.contactMobile === ''">发送验证码</Button>
+            </FormItem>
+            <FormItem label="验证码">
+                <Input v-model="bizLicenseForm.verifyCode"  placeholder="验证码"/>
+            </FormItem>
+            <Button type="success" @click="handleSubmit" long :disabled="!submitEnabled">提交</Button>
+        </Form>
+        <h2 v-show="!formShow">
+            你的申请我们已经收到, 客户经理会尽快与您联系, 感谢您的关注!
+
+        </h2>
+    </div>
+
 </template>
 
 <script>
@@ -74,6 +81,7 @@
         name: 'loan-apply-biz',
         data () {
             return {
+                formShow: true,
                 submitEnabled: false,
                 uploadAction: `${util.baseUrl}/loan/bizlic/ocr`,
                 ocrBtnText: '或 上传营业执照自动识别',
