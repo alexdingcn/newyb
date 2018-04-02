0c98869f42c9126c72fbbdbda47ff0df6556b743
lanzhu259X <liliang259X@163.com>
2018-04-02 09:04:34 +0800

    job configuration

--- job/src/main/java/com/yiban/erp/executor/JobApp.java
+++ job/src/main/java/com/yiban/erp/executor/JobApp.java
@@ -12,32 +12,6 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableAutoConfiguration
 public class JobApp {
 
-    @Value("${xxl.job.admin.addresses}")
-    private String addresses;
-    @Value("${xxl.job.executor.appname}")
-    private String appname;
-    @Value("${xxl.job.executor.ip}")
-    private String ip;
-    @Value("${xxl.job.executor.port}")
-    private int port;
-    @Value("${xxl.job.executor.logpath}")
-    private String logpath;
-    @Value("${xxl.job.accessToken}")
-    private String accessToken;
-
-    @Bean(initMethod = "start", destroyMethod = "destroy")
-    public XxlJobExecutor xxlJobExecutor() {
-        System.out.println(">>>>>>>>>>> xxl-job config init. appname:" + appname + ", address:" + addresses);
-        XxlJobExecutor xxlJobExecutor = new XxlJobExecutor();
-        xxlJobExecutor.setIp(ip);
-        xxlJobExecutor.setPort(port);
-        xxlJobExecutor.setAppName(appname);
-        xxlJobExecutor.setAdminAddresses(addresses);
-        xxlJobExecutor.setLogPath(logpath);
-        xxlJobExecutor.setAccessToken(accessToken);
-        return xxlJobExecutor;
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(JobApp.class);
     }
