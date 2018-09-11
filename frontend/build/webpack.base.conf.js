5e264dd30f06fe2c1bacc7e91aeb0c75dd1ecc7e
kkwkk <734622029@qq.com>
2018-09-11 17:43:40 +0800

    pack

---
+++ frontend/build/webpack.base.conf.js
@@ -0,0 +1,144 @@
+"use strict";
+const path = require("path");
+const utils = require("./utils");
+const config = require("../config");
+const os = require("os");
+const vueLoaderConfig = require("./vue-loader.conf");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HappyPack = require("happypack");
+var happyThreadPool = HappyPack.ThreadPool({ size: os.cpus().length });
+
+function resolve(dir) {
+  return path.join(__dirname, "..", dir);
+}
+
+const createLintingRule = () => ({
+  test: /\.(js|vue)$/,
+  loader: "eslint-loader",
+  enforce: "pre",
+  include: [resolve("src"), resolve("test")],
+  options: {
+    formatter: require("eslint-friendly-formatter"),
+    emitWarning: !config.dev.showEslintErrorsInOverlay
+  }
+});
+
+module.exports = {
+  context: path.resolve(__dirname, "../"),
+  entry: {
+    app: "./src/main.js"
+  },
+  output: {
+    path: config.build.assetsRoot,
+    filename: "[name].js",
+    publicPath:
+      process.env.NODE_ENV === "production"
+        ? config.build.assetsPublicPath
+        : config.dev.assetsPublicPath
+  },
+  resolve: {
+    extensions: [".js", ".vue", ".json"],
+    alias: {
+      vue$: "vue/dist/vue.esm.js",
+      "@": resolve("src")
+    }
+  },
+  module: {
+    rules: [
+      // ...(config.dev.useEslint ? [createLintingRule()] : []),
+      {
+        test: /\.vue$/,
+        use: [
+          {
+            loader: "vue-loader",
+            options: vueLoaderConfig
+          },
+          {
+            loader: "iview-loader",
+            options: {
+              prefix: false
+            }
+          }
+        ]
+      },
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        include: [
+          resolve("src"),
+          resolve("test"),
+          resolve("node_modules/webpack-dev-server/client")
+        ]
+      },
+      {
+        test: /iview\/.*?js$/,
+        loader: "happypack/loader?id=happybabel",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          use: ["css-loader?minimize", "autoprefixer-loader"],
+          fallback: "style-loader"
+        }),
+        exclude: /node_modules/
+      },
+      {
+        test: /\.less$/,
+        use: ExtractTextPlugin.extract({
+          use: ["css-loader?minimize", "autoprefixer-loader", "less-loader"],
+          fallback: "style-loader"
+        })
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|webp)(\?.*)?$/,
+        loader: "url-loader",
+        options: {
+          limit: 10000,
+          name: utils.assetsPath("img/[name].[hash:7].[ext]")
+        }
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        loader: "url-loader",
+        options: {
+          limit: 10000,
+          name: utils.assetsPath("media/[name].[hash:7].[ext]")
+        }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: "url-loader",
+        options: {
+          limit: 10000,
+          name: utils.assetsPath("fonts/[name].[hash:7].[ext]")
+        }
+      },
+      {
+        test: /\.(html|tpl)$/,
+        loader: "html-loader"
+      }
+    ]
+  },
+  plugins: [
+    new HappyPack({
+      id: "happybabel",
+      loaders: ["babel-loader"],
+      threadPool: happyThreadPool,
+      cache: true,
+      verbose: true
+    })
+  ],
+  node: {
+    // prevent webpack from injecting useless setImmediate polyfill because Vue
+    // source contains it (although only uses it if it's native).
+    setImmediate: false,
+    // prevent webpack from injecting mocks to Node native modules
+    // that does not make sense for the client
+    dgram: "empty",
+    fs: "empty",
+    net: "empty",
+    tls: "empty",
+    child_process: "empty"
+  }
+};
